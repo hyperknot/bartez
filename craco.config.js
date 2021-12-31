c2d79679586b59aaf71a5cabab8d16a5c5c0d063
Zsolt Ero <zsolt.ero@gmail.com>
2021-12-31 20:17:54 +0100

    CRA 5.x

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,35 @@
 module.exports = {
+  webpack: {
+    alias: {},
+    plugins: {
+      add: [],
+    },
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        path: require.resolve('path-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        url: require.resolve('url/'),
+        stream: require.resolve('stream-browserify'),
+      }
+      webpackConfig.stats = {
+        children: true,
+        errorDetails: true,
+      }
+      return webpackConfig
+    },
+  },
   babel: {
     plugins: [
       ['@babel/plugin-proposal-decorators', { legacy: true }],
       ['@babel/plugin-proposal-class-properties', { loose: false }],
+      ['@babel/plugin-proposal-private-methods', { loose: false }],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
     ],
-    // Babel >= 7.13.0 (https://babeljs.io/docs/en/assumptions)
-    assumptions: {
-      setPublicClassFields: false,
-    },
+    // // Babel >= 7.13.0 (https://babeljs.io/docs/en/assumptions)
+    // assumptions: {
+    //   setPublicClassFields: false,
+    // },
   },
 }
