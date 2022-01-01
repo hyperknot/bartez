3d36365cb86728671c119ceef484058c0402e4ac
Zsolt Ero <zsolt.ero@gmail.com>
2022-01-01 22:14:39 +0100

    Revert "Revert "remove unused import""
    This reverts commit 8d958146e2c82a0711613d6cb7888e53b2fa7d01.

--- src/stores/contractStore.js
+++ src/stores/contractStore.js
@@ -1,5 +1,5 @@
 import { action, makeObservable, observable } from 'mobx'
-import { getCachedURL, getTokenMetadata } from '../utils'
+import { getCachedURL } from '../utils'
 import { userStore } from './userStore'
 
 class ContractStore {
